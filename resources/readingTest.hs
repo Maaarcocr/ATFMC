8a9c306f74177030dd985c944bdbcc303c4fa134
Marco Concetto Rudilosso <marcoconcettorudilosso@gmail.com>
2016-01-04 15:15:24 +0100

    no indentation

--- resources/readingTest.hs
+++ resources/readingTest.hs
@@ -2,13 +2,13 @@ import System.IO
 import System.Directory
 
 main = do
-	handle <- openFile "./resources/testText.txt" ReadMode
-	(tempName, tempHandle) <- openTempFile "." "temp" 
-	reading <- hGetContents handle
-	let numberOfEdges = read $ (lines reading) !! 2
-	putStr $ unlines $ take (6+numberOfEdges) $ lines reading
-	hPutStr tempHandle (unlines $ drop (6+numberOfEdges) $ lines reading)
-	hClose handle
-	hClose tempHandle
-	removeFile "./resources/testText.txt"
-	renameFile tempName "./resources/testText.txt"
+  handle <- openFile "./resources/testText.txt" ReadMode
+  (tempName, tempHandle) <- openTempFile "." "temp" 
+  reading <- hGetContents handle
+  let numberOfEdges = read $ (lines reading) !! 2
+  putStr $ unlines $ take (6+numberOfEdges) $ lines reading
+  hPutStr tempHandle (unlines $ drop (6+numberOfEdges) $ lines reading)
+  hClose handle
+  hClose tempHandle
+  removeFile "./resources/testText.txt"
+  renameFile tempName "./resources/testText.txt"
