50cc9db19f11f8179171ad03aef50996ffcbe7f7
Maaarcocr <marcoconcettorudilosso@gmail.com>
2016-01-06 12:02:04 +0100

    23 tests, added Robin Hirsch tests

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ ghc --make readingTest
 ghc --make verifier
 cd ..
 COUNTER=0
-while [ $COUNTER -lt 17 ]; do
+while [ $COUNTER -lt 23 ]; do
 	let COUNTER=COUNTER+1
 	echo test_number: $COUNTER
 	./resources/readingTest | ./graph | ./resources/verifier
